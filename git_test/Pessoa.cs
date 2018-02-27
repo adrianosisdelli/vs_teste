e693ea273b698ee69a8dbe70ae3ce8224cd1ca8f
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-02-27 00:19:05 -0300

    Criação e definição inicial da classe Pessoa

---
+++ git_test/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_test
+{
+    public class Pessoa
+    {
+        public string nome { get; set; }
+        public string endereco { get; set; }
+    }
+}
